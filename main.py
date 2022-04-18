4b7ae57ec50594916d75838bb0137bb9b7046751
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-04-18 13:07:03 +0200

    Render simple status with details

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import flask
+import markupsafe
 
 app = flask.Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def go():
+    return _render_status(is_ok=True, details="…")
+
+
+def _render_status(*, is_ok, details):
+    heading = (
+        '<h1 style="color: green">OK</h1>'
+        if is_ok
+        else '<h1 style="color: red">NOK</h1>'
+    )
+    return f"""<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Status</title>
+  </head>
+  <body>
+    {heading}
+    <pre>{markupsafe.escape(details)}</pre>
+  </body>
+</html>
+"""
