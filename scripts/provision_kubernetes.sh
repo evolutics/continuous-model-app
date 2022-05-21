8aeed7218210a3c8ca4fa891e933a7e711db5f46
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-05-21 15:44:38 +0200

    Use Kerek for continuous building, testing and deploying

--- scripts/provision_kubernetes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-set -o nounset
-set -o pipefail
-
-curl --fail --location --silent https://get.k3s.io | sh -
--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -4,4 +4,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-curl --fail --show-error "http://$1:8080"
+curl --fail --max-time 3 --retry 99 --retry-connrefused --retry-max-time 150 \
+  --show-error http://"${KEREK_IP}":8080
