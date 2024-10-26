3f7fedbedc06da1fd48af65e1a0e1fa9d10f993e
Benjamin Fischer <benjamin.fischer@evolutics.info>
2024-10-26 15:29:08 +0200

    Shorten

--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
+set -o errexit -o nounset -o pipefail
 
 curl --fail --max-time 3 --retry 99 --retry-connrefused --retry-max-time 150 \
   --show-error http://"${KEREK_IP_ADDRESS}":8080
